da89329a712140917730576f7ddbe2e8e164dc3b
Víctor Rampérez Martín <vramperez@MacBook-Air-de-Victor.local>
2025-12-11 11:58:42 +0100

    fix(mysql-migration): fix add dcql mysql migration

--- src/main/resources/db/migration/MYSQL/V2_0_2__add_dcql.sql
+++ src/main/resources/db/migration/MYSQL/V2_0_2__add_dcql.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `scope_entry` ADD `dcql_query` LONGTEXT;
-ALTER TABLE `scope_entry` MODIFY COLUMN `presentation_definition` LONGTEXT NULL DEFAULT NULL;
+ALTER TABLE `scope_entry` MODIFY `presentation_definition` LONGTEXT NULL;
