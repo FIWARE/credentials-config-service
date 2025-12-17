3ef6fb5b95a9d503a9abf120847b44407742104f
Miguel Ortega <miguel.ortega@seamware.com>
2025-12-17 09:47:03 +0100

    refactor(db): replace flyway with liquibase
    Liquibase has better support for different types of databases. It allows having a single migration file for all databases.

--- src/main/resources/db/migration/MYSQL/V2_0_2__add_dcql.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `scope_entry` ADD `dcql_query` LONGTEXT;
-ALTER TABLE `scope_entry` MODIFY `presentation_definition` LONGTEXT NULL;
--- src/main/resources/db/migration/POSTGRES/V2_0_2__add_dcql.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE scope_entry ADD COLUMN dcql_query TEXT;
-ALTER TABLE scope_entry ALTER COLUMN presentation_definition DROP NOT NULL;
