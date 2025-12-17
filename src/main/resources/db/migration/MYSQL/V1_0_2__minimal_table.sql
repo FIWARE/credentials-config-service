1329ad6bc43fc43cf380706fe746c116ba044937
Miguel Ortega <miguel.ortega@seamware.com>
2025-12-17 09:47:03 +0100

    refactor(db): replace flyway with liquibase
    Liquibase has better support for different types of databases. It allows having a single migration file for all databases.

--- src/main/resources/db/migration/MYSQL/V1_0_2__minimal_table.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `endpoint_entry` CASCADE;
-DROP TABLE IF EXISTS `credential` CASCADE;
-DROP TABLE IF EXISTS `service` CASCADE;
-
-CREATE TABLE IF NOT EXISTS `service` (
-    `id` varchar(255) NOT NULL PRIMARY KEY,
-    `default_oidc_scope` varchar(255) NOT NULL,
-    `oidc_scopes` LONGTEXT NOT NULL
-);
